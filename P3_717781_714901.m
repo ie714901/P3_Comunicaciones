ef40385e1a578de8e646527aa93e0590ddab0ae8
lulucarstensen <72319324+lulucarstensen@users.noreply.github.com>
2022-12-04 21:58:02 -0600

    Complete audio and image transmission

--- P3_717781_714901.m
+++ P3_717781_714901.m
@@ -1,14 +1,12 @@
-%% Transmisión Práctica 3
+%% Transmisión Práctica 3 SRRC
 
+preamble_imagen = [1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0]';
 
+SFD_imagen= [1 0 1 0 1 0 1 1]';
 
-preamble = [1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0]';
+DSA_imagen = de2bi(uint8('Hola buenas tardes 1998'),8,'left-msb');%DSA size obtention
 
-SFD= [1 0 1 0 1 0 1 1]';
-
-DSA = de2bi(uint8('Pr??ctica 2 FASE II: 717781 y 714901'),8,'left-msb');%DSA size obtention
-
-DSA = reshape(DSA',1,numel(DSA));%Reshapes DSA to a transposed vector of 1x its size 
+DSA_imagen = reshape(DSA_imagen',1,numel(DSA_imagen));%Reshapes DSA to a transposed vector of 1x its size 
 
 load lena512.mat; 
 
@@ -20,100 +18,26 @@ imshow(img);
 
 size_img=de2bi(size(img),16,'left-msb');%Image's size in 16 bits
 
-header= [size_img(1,:) size_img(2,:)]';
+header_imagen = [size_img(1,:) size_img(2,:)]';
 
-payload = de2bi(img,8,'left-msb'); % Puede ser right-msb
+payload_imagen = de2bi(img,8,'left-msb'); % Puede ser right-msb
 
-payload = payload';
+payload_imagen = payload_imagen';
 
-payload = payload(:);%Payload concatenation
+payload_imagen = payload_imagen(:);%Payload concatenation
 
-bits2Tx = [preamble; SFD; DSA'; header; payload];%Concatenation of complete set of bits
-
-%% Pulse SRRC
-
-Fs = 96000;
-
-Ts = 1/Fs;
-
-beta = 0.2;%Roll-off
-
-B = 20000; %Bandwidth
-
-Rb = 2*B / (1 + beta);%Data Rate 
-
-mp = ceil(Fs/Rb);%Rounds up mp to the next highest integer
-
-Rb = Fs/mp;
-
-Tp = 1/Rb; %Transmision period of symbol
-
-B = (Rb*(1+beta)/2);%Occupied BW
-
-D = 6;%Pulse Duration
-
-type = 'srrc';
-
-E = Tp;%Energy
-
-[pbase ~] = rcpulse(beta,D,Tp,Ts,type,E);%Gets pulse
-
-%% Pulse Train SRRC
-
-pnrz = pbase; % RC or SRRC
-s1 = int8(bits2Tx); %store bits value in other variable
-s1(s1 == 0) = -1; %switch all zeros to -1
-s = zeros(1, numel(s1)*mp); %zero's vector of s1*mp
-s(1:mp:end) = s1; %Impulse train
-pulse_train = conv(pnrz, s); %convolution of pnrz and s
-
-%% SQUARE PULSE
-
-mp = 3;
-Fs = 96000;
+bits2Tx_imagen = [preamble_imagen; SFD_imagen; DSA_imagen'; header_imagen; payload_imagen];%Concatenation of complete set of bits
 
 
-pnrz = rectwin(mp); % Complete pulse
-s1 = int8(bits2Tx); %store bits value in other variable
-s1(s1 == 0) = -1; %switch all zeros to -1
-s = zeros(1, numel(s1)*mp); %zero's vector of s1*mp
-s(1:mp:end) = s1; %Impulse train
-pulse_train = conv(pnrz, s); %convolution of pnrz and s
+% Transmisión Audio
 
-pwelch(pulse_train);
+preamble_audio = [1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0]';
 
-%% Signal normalization
-pow = sum(pulse_train.*pulse_train)/numel(pulse_train);
-pulse_train = pulse_train/sqrt(pow);
-pow = sum(pulse_train.*pulse_train)/numel(pulse_train);
+SFD_audio = [1 0 1 0 1 0 1 1]';
 
-%% Time and Freq. Charts of pulse and pulse train
-delay = (D*mp)/2;
-num_samples = 20;
-window = mp*num_samples+delay;
-t = 0:Ts:window*Ts-Ts;
-% figure; set(stem(t, pulse_train(1:window)),'Marker','none'); % By time
-figure; set(stem(pulse_train(1:window)),'Marker','none'); % By samples
-xlabel('Samples'); ylabel('Amplitude'); title('Generated Line Code 4B/5B MLT-3');
+DSA_audio = de2bi(uint8('Pr??ctica 2 FASE II: 717781 y 714901'),8,'left-msb');%DSA size obtention
 
-%% PUNTO 8
-eyediagram(pulse_train(5000:20000),2*mp);%Eye Digram of Pulse Train to transmit
-title('EyeDiagram SRRC')
-
-%% PUNTO 9
-
-soundsc( [zeros(1,Fs/2) pulse_train], Fs );%Transmision of pulse with silence at the beginning
-
-
-%% TRANSMISIÓN DE AUDIO %%%%%%%%%
-
-preamble = [1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0]';
-
-SFD= [1 0 1 0 1 0 1 1]';
-
-DSA = de2bi(uint8('Pr??ctica 2 FASE II: 717781 y 714901'),8,'left-msb');%DSA size obtention
-
-DSA = reshape(DSA',1,numel(DSA));%Reshapes DSA to a transposed vector of 1x its size 
+DSA_audio = reshape(DSA_audio',1,numel(DSA_audio));%Reshapes DSA to a transposed vector of 1x its size 
 
 filename = 'song3.opus';
 
@@ -132,18 +56,22 @@ b = b'; bits_audio = b(:); % Arrange to a single vector
 size_audio = de2bi(numel(bits_audio),32,'left-msb');
 
 %header = [size_audio(1,:) 1]';
+header_audio = [size_audio(:)];
 
-header = [size_audio(:)];
+payload_audio = de2bi(songV,8,'left-msb'); % Puede ser right-msb
 
-payload = de2bi(songV,8,'left-msb'); % Puede ser right-msb
+payload_audio = payload_audio';
 
-payload = payload';
+payload_audio = payload_audio(:);%Payload concatenation
 
-payload = payload(:);%Payload concatenation
+bits2Tx_audio = [preamble_audio; SFD_audio; DSA_audio'; header_audio; payload_audio];%Concatenation of complete set of bits
 
-bits2Tx = [preamble; SFD; DSA'; header; payload];%Concatenation of complete set of bits
 
-%% Pulse SRRC
+% bits2Tx IMAGE AND AUDIO
+
+bits2Tx = [preamble_imagen; SFD_imagen; DSA_imagen'; header_imagen; payload_imagen; preamble_audio; SFD_audio; DSA_audio'; header_audio; payload_audio];
+
+% % Pulse SRRC
 
 Fs = 96000;
 
@@ -171,40 +99,30 @@ E = Tp;%Energy
 
 [pbase ~] = rcpulse(beta,D,Tp,Ts,type,E);%Gets pulse
 
-%% Pulse Train SRRC
+% % IMAGE AND AUDIO PULSE TRAIN SRRC 
 
 pnrz = pbase; % RC or SRRC
 s1 = int8(bits2Tx); %store bits value in other variable
 s1(s1 == 0) = -1; %switch all zeros to -1
-s = zeros(1, numel(s1)*mp); %zero's vector of s1*mp
-s(1:mp:end) = s1; %Impulse train
-pulse_train = conv(pnrz, s); %convolution of pnrz and s
-
-%% SQUARE PULSE
-
-mp = 3;
-Fs = 96000;
+s_imagen = zeros(1, numel(s1)*mp); %zero's vector of s1*mp
+s_imagen(1:mp:end) = s1; %Impulse train
+pulse_train_imagen = conv(pnrz, s_imagen); %convolution of pnrz and s
 
 
-pnrz = rectwin(mp); % Complete pulse
-s1 = int8(bits2Tx); %store bits value in other variable
-s1(s1 == 0) = -1; %switch all zeros to -1
-s = zeros(1, numel(s1)*mp); %zero's vector of s1*mp
-s(1:mp:end) = s1; %Impulse train
-pulse_train = conv(pnrz, s); %convolution of pnrz and s
+% % NORMALIZATION
 
-pwelch(pulse_train);
-
-%% Signal normalization
 pow = sum(pulse_train.*pulse_train)/numel(pulse_train);
 pulse_train = pulse_train/sqrt(pow);
 pow = sum(pulse_train.*pulse_train)/numel(pulse_train);
 
+% % EYE DIAGRAM
 
-%% EYE DIAGRAM
-eyediagram(pulse_train(1:5000),2*mp);%Eye Digram of Pulse Train to transmit
-title('EyeDiagram SRRC')
+eyediagram(pulse_train(5000:20000),2*mp);%Eye Diagram of Pulse Train to transmit
+title('EyeDiagram Image SRRC')
 
-%% TRANSMIT AUDIO
+
+
+% % TRANSMIT 
 
 soundsc( [zeros(1,Fs/2) pulse_train], Fs );%Transmision of pulse with silence at the beginning
+
