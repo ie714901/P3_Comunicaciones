6c1e77656160d8ce2f28741f997d00dec72d91a1
ie714901 <55028504+ie714901@users.noreply.github.com>
2022-12-04 22:45:38 -0600

    Rx imagen y audio - img recibida, audio no

---
+++ Rx_completo.m
@@ -0,0 +1,116 @@
+Fs=96000; Nb=16;Chs=1; 
+recObj = audiorecorder(Fs, Nb, Chs); 
+get(recObj); 
+disp('Start speaking.') 
+recordblocking(recObj, 80); 
+disp('End of Recording.'); 
+% Store data in double-pre cision array. 
+myRecording = getaudiodata(recObj); 
+% Plot the waveform. 
+plot(myRecording); 
+% Power Spectrum Densitiy: 
+pwelch(myRecording,500,300,500,'one-side','power',Fs) 
+
+% Elimine de la señal recibida, la parte que corresponde al silencio          
+Rx_signal = myRecording;
+%Rx_signal = y;   
+threshold = 0.1;                            % Detecting the channel energization 
+start = find(abs(Rx_signal)> threshold,3,'first'); % Initial 
+stop  = find(abs(Rx_signal)> threshold,1,'last');  % End 
+Rx_signal = Rx_signal (start:stop);
+figure();
+plot(Rx_signal); title('Señal Rx sin silencio al principio');
+
+% Pulso base para CL srrc
+Fs      =   96000;            % Samples per second
+Ts      =   1/Fs;              % 
+beta    =   0.2;               % Roll-off factor
+B       =   20000;             % Bandwidth available
+Rb      =   2*B/(1+beta);      % Bit rate (= Baud rate)
+mp      =   ceil(Fs/Rb);       % samples per pulse
+Rb      =   Fs/mp;             % Recompute bit rate
+Tp      =   1/Rb;              % Symbol period
+B       =   (Rb*(1+beta)/2);   % Bandwidth consumed
+D       =   6;                 % Time duration in terms of Tp
+type    =   'srrc';            % Shape pulse: Square Root Rise Cosine
+E       =   Tp;                % Energy
+[pbase ~] = rcpulse(beta, D, Tp, Ts, type, E);    % Pulse Generation
+
+% Match Filter
+Match_Filter = fliplr(pbase);  %Mirror the rectangular pulse base
+Rx_signal_filtered = conv(Rx_signal, Match_Filter); %Convolution of filtered signal and mirrored rectangular pulse base + noise
+figure();
+plot(Rx_signal_filtered); title('Rx con Match Filter');
+
+% Diagrama de ojo
+figure();
+eyediagram(Rx_signal_filtered(1:100000), 3*mp); %Eye Diagram
+
+% Densidad espectral de potencia
+pwelch(Rx_signal_filtered,500,300,500,Fs,'power'); %Spectral density 
+
+% Sincronización Early-Late srrc
+delay = (D*mp)/2;
+symSync = comm.SymbolSynchronizer('TimingErrorDetector','Early-Late (non-data-aided)','SamplesPerSymbol',mp);
+rxSym = symSync(-Rx_signal_filtered(delay:end));
+release(symSync); % Liberar el objeto
+scatterplot(rxSym); title('Scatterplot of Syncronized Signal');
+%Delete the extra bit given by the syncronization
+rec_line_code = rxSym(2:end);
+
+% Convertir a bits
+sym = sign(rec_line_code);
+bits_Rx = (sym + 1) / 2; %Each bit take the value of 1 or 0
+bits_Rx = bits_Rx'; %transpose
+bits_Rx = bits_Rx(:);
+
+% Creación del objeto
+preamble = [1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0]';
+preamble_detect = comm.PreambleDetector(preamble,'Input','Bit');
+SFD = [1 0 1 0 1 0 1 1]';
+% Preamble detection. The index shows where the frame ends
+idx = preamble_detect(bits_Rx(1:128))  % 128 bits window
+% Once found the index, discard "junk bits", as follow
+bits_Rx= bits_Rx(idx+1-numel(preamble):end);
+
+%Sizes
+preamble_size = numel(preamble);
+SFD_size = numel(SFD);
+
+% SFD
+SFD_bits = bits_Rx(preamble_size+1:preamble_size+numel(SFD));
+%Destination and Source Address
+DSA_bits = bits_Rx(preamble_size+numel(SFD)+1:preamble_size+numel(SFD)+184);
+DSA_size = numel(DSA_bits);
+DSA_val = reshape(DSA_bits, 8, DSA_size/8)'; 
+DSA_val = bi2de(DSA_val, 'left-msb'); 
+DSA_val = char(DSA_val)' %print DSA in console
+
+header = bits_Rx(preamble_size+SFD_size+DSA_size+1:preamble_size+SFD_size+DSA_size+32); %header
+header_size = numel(header);
+w = bi2de(header(1:16)','left-msb'); %image's width
+h = bi2de(header(17:header_size)','left-msb'); %image's height
+data_bits = bits_Rx(preamble_size+SFD_size+DSA_size+header_size+1 ...
+    :preamble_size+SFD_size+DSA_size+header_size+w*h*8); %data
+
+bits_reshape = reshape(data_bits, 8, w*h); %Matrix of 8 * w * h
+bits_reshape = bits_reshape'; %Transpose 
+decVal = bi2de(bits_reshape, 'left-msb'); %convert from binary to decimal
+lena_reshape = reshape(decVal, w, h); %reshape the image
+figure();
+imshow(uint8(lena_reshape)); %show the reconstructed image
+title('Lena Reshape');
+
+% Construir y escribir archivo de audio
+SFD_bits = bits_Rx(57:56+numel(SFD));
+
+%Destination and Source Address
+DSA_bits = bits_Rx(56+numel(SFD)+1:56+numel(SFD)+288);
+
+header = bits_Rx(56+8+288+1:56+8+288+32); %header
+data_bits = bits_Rx(384+1:502104); %data
+
+audioValues = vec2mat(data_bits,8); % Obtain Bytes
+audioValues = bi2de(audioValues); % Bin2Dec conversion
+rxAudioId = fopen('rx_audio.opus','w'); %File ID;
+fwrite(rxAudioId,audioValues); % Write the file
